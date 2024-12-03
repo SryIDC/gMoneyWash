015383a46715b0357af727a903e39ef33ca3403f
Gigo <iniyan.at2k7@gmail.com>
2024-12-03 17:53:31 +0530

    Updated Client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,16 +1,8 @@
 local ActivityPed
 local Washing = false
-local models = {
-    "g_m_importexport_01",
-    "g_m_m_armgoon_01",
-    "g_m_y_armgoon_02",
-    "g_m_y_ballaorig_01",
-    "g_m_y_ballaeast_01",
-    "g_m_y_ballasout_01",
-    "g_m_y_famca_01"
-}
-
+local Washer = "bkr_prop_prtmachine_dryer_spin"
 CreateThread(function()
+    lib.requestModel(Washer, 5000)
     DealerPed()
 end)
 
@@ -19,7 +11,7 @@ function DealerPed()
     local spawn = ped.Spawn
     lib.requestModel(ped.Model, 5000)
     local hash = GetHashKey(ped.Model)
-    Dealer = CreatePed("PED_TYPE_CIVMALE", hash, spawn.x, spawn.y, spawn.z - 0.98, spawn.w, false, false)
+    Dealer = CreatePed("PED_TYPE_CIVMALE", hash, spawn.x, spawn.y, spawn.z - 1, spawn.w, false, false)
     TaskSetBlockingOfNonTemporaryEvents(Dealer, true)
     FreezeEntityPosition(Dealer, true)
     SetEntityInvincible(Dealer, true)
@@ -29,13 +21,17 @@ function DealerPed()
             label = "I want to wash some money",
             icon = "fa-solid fa-person",
             canInteract = function()
-                local item = exports.ox_inventory:Search('count', Config.BlackMoney)
                 if not Washing then return true end
             end,
             onSelect = function()
                 local item = exports.ox_inventory:Search('count', Config.BlackMoney)
-                if item < 1000 then return lib.notify({ description =
-                    "You must have atleast more than $1000 to exchange money!", type = "error" }) end
+                if item < 1000 then
+                    return lib.notify({
+                        description =
+                        "You must have atleast more than $1000 to exchange money!",
+                        type = "error"
+                    })
+                end
                 lib.notify({ description = "Wait for the dealer to give you the location" })
                 SetTimeout(5000, function()
                     lib.notify({ description = "Go to the dealer and exchange your money", duration = 5000 })
@@ -46,18 +42,23 @@ function DealerPed()
     })
 end
 
-function StartActivity(type)
-    local randped = math.random(1, #models)
-    local actped = models[randped]
-    lib.requestModel(actped, 5000)
+function StartActivity()
     local hash = GetHashKey(actped)
     local randspawn = math.random(1, #Config.Locations)
     local spawn = Config.Locations[randspawn]
 
     ActivityPed = CreatePed("PED_TYPE_MISSION", hash, spawn.x, spawn.y, spawn.z - 0.98, spawn.w, false, false)
-    TaskSetBlockingOfNonTemporaryEvents(ActivityPed, true)
-    SetEntityInvincible(ActivityPed, true)
-    ActivityPedBlip = CreateRoute(spawn, "Hawala Exchange")
+    Machine = CreateObject(GetHashKey(Washer), spawn.x, spawn.y, spawn.z, false, false, false)
+    SetEntityHeading(Machine, spawn.w)
+    blip = AddBlipForEntity(Machine)
+    SetBlipSprite(blip, 1)
+    SetBlipDisplay(blip, 2)
+    SetBlipScale(blip, 0.8)
+    SetBlipColour(blip, 66)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(label)
+    EndTextCommandSetBlipName(blip)
 
     exports.ox_target:addLocalEntity(ActivityPed, {
         {
@@ -94,48 +95,40 @@ function WashMoney()
                 car = true,
             }
         }) then
+        if DoesBlipExist(blip) then
+            RemoveBlip(blip)
+        end
         TriggerServerEvent("MoneyWash:server:removeItem", Config.BlackMoney, item)
         TriggerServerEvent("MoneyWash:server:additem", Config.Account, washedmoney)
-        lib.notify({ description = "$" ..
-        item ..
-        " has been exchanged and $" ..
-        deduction .. " has been deducted as commission and $" .. washedmoney .. " has been given after washing!", duration = 8000, icon =
-        "fa-solid fa-money-bill" })
+        lib.notify({
+            description = "$" ..
+                item ..
+                " has been exchanged and $" ..
+                deduction .. " has been deducted as commission and $" .. washedmoney .. " has been given after washing!",
+            duration = 8000,
+            icon =
+            "fa-solid fa-money-bill"
+        })
         exports.ox_target:removeLocalEntity(ActivityPed, "MoneyWash:exchange")
         SetTimeout(5000, function()
-            if DoesEntityExist(ActivityPed) then
-                DeleteEntity(ActivityPed)
-            end
-            if DoesBlipExist(ActivityPedBlip) then
-                RemoveBlip(ActivityPedBlip)
+            if DoesEntityExist(Machine) then
+                DeleteEntity(Machine)
             end
             Washing = false
         end)
     end
 end
 
-function CreateRoute(coords, label)
-    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
-    SetBlipSprite(blip, 1)
-    SetBlipDisplay(blip, 2)
-    SetBlipScale(blip, 0.8)
-    SetBlipColour(blip, 66)
-    SetBlipAsShortRange(blip, true)
-    BeginTextCommandSetBlipName("STRING")
-    AddTextComponentString(label)
-    EndTextCommandSetBlipName(blip)
-    SetBlipRoute(blip, true)
-    SetBlipRouteColour(blip, 66)
-    return blip
-end
-
 AddEventHandler("onResourceStop", function(resourceName)
     if GetCurrentResourceName == resourceName then
-        if DoesEntityExist(Ped) then
-            DeleteEntity(Ped)
+        if DoesEntityExist(Dealer) then
+            DeleteEntity(Dealer)
         end
-        if DoesBlipExist(jobped) then
-            RemoveBlip(jobped)
+        if DoesBlipExist(blip) then
+            RemoveBlip(blip)
+        end
+        if DoesEntityExist(Machine) then
+            DeleteEntity(Machine)
         end
     end
 end)
