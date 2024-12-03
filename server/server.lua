19715a47f068b4cf677c24b5f692b40f62e94172
Gigo <iniyan.at2k7@gmail.com>
2024-12-03 17:54:47 +0530

    Removed unwanted code

--- server/server.lua
+++ server/server.lua
@@ -1,19 +1,3 @@
-lib.callback.register('mosleys:server:SpawnJobVeh', function(source, model, coords)
-    local ped = GetPlayerPed(source)
-    local id, vehicle = qbx.spawnVehicle({
-        model = tonumber(model) or joaat(model),
-        spawnSource = coords,
-        warp = false,
-
-        props = {
-            fuelLevel = 100.00,
-        }
-
-    })
-    exports.qbx_vehiclekeys:GiveKeys(source, vehicle)
-    return id
-end)
-
 RegisterNetEvent("MoneyWash:server:additem", function (account, amount)
     local src = source
     exports.ox_inventory:AddItem(src, account, amount)
